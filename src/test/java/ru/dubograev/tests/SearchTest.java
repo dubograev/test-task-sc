91210ea5a5e599b2ed5dba4644b7d4085398e0d0
dubograev <a.dubograev@gmail.com>
2022-11-02 22:10:02 +0300

    fixed several test cases

--- src/test/java/ru/dubograev/tests/SearchTest.java
+++ src/test/java/ru/dubograev/tests/SearchTest.java
@@ -1,11 +1,17 @@
 package ru.dubograev.tests;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import io.appium.java_client.AppiumBy;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
 public class SearchTest extends TestBase{
@@ -13,25 +19,29 @@ public class SearchTest extends TestBase{
     @DisplayName("Sorting test")
     void sortingTest() {
         step("Skip welcome screens", () -> {
-            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageButton")).click();
-            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.ImageButton")).click();
+            $(AppiumBy.id("ru.sportmaster.app:id/toolbar"))
+                    .$(AppiumBy.className("android.widget.ImageButton")).click();
+            $(AppiumBy.id("ru.sportmaster.app:id/toolbar"))
+                    .$(AppiumBy.className("android.widget.ImageButton")).click();
             $(AppiumBy.id("com.android.permissioncontroller:id/permission_allow_one_time_button")).click();
         });
 
         step("Select the city", () -> {
             $(AppiumBy.id("ru.sportmaster.app:id/editText")).click();
-            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ViewFlipper/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]")).click();
+            $(AppiumBy.id("ru.sportmaster.app:id/recyclerView"))
+                    .$$(AppiumBy.className("android.view.ViewGroup")).first().click();
         });
 
         step("Search for a product", ()-> {
-            $(AppiumBy.id("ru.sportmaster.app:id/editText")).click();
-            $(AppiumBy.id("ru.sportmaster.app:id/editText")).sendKeys("reebok");
-            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.view.View")).click();
+            $(AppiumBy.xpath("//*[@resource-id='ru.sportmaster.app:id/editText']")).click();
+            $(AppiumBy.xpath("//*[@resource-id='ru.sportmaster.app:id/editText']")).sendKeys("reebok");
+            $(AppiumBy.xpath("//*[@resource-id='ru.sportmaster.app:id/viewHintClickArea']")).click();
         });
 
         step("Switch the sorting to 'По возрастанию цены'", () -> {
             $(AppiumBy.id("ru.sportmaster.app:id/textViewSorting")).click();
-            $(AppiumBy.className("androidx.recyclerview.widget.RecyclerView")).$$(AppiumBy.className("android.view.ViewGroup")).get(3).click();
+            $(AppiumBy.className("androidx.recyclerview.widget.RecyclerView"))
+                    .$$(AppiumBy.className("android.view.ViewGroup")).get(3).click();
         });
 
         step("Check that sorting button name changed to 'По возрастанию цены'", ()-> {
@@ -39,8 +49,23 @@ public class SearchTest extends TestBase{
         });
 
         step("Check that first item has the minimum price", ()-> {
-            $(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ViewFlipper/android.widget.ScrollView/android.widget.ViewFlipper/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[2]")).shouldHave(Condition.text("999 ₽"));
+            Assertions.assertThat(getMinPrice()).isEqualTo(getFirstElementPrice());
         });
+    }
 
+    int getMinPrice() {
+        ElementsCollection elements = $$(AppiumBy.xpath("//*[@resource-id='ru.sportmaster.app:id/textViewMainPrice']"));
+        List<Integer> listOfPrices = new ArrayList();
+        for (int i = 0; i < elements.size(); i++) {
+            listOfPrices.add(Integer.parseInt(elements.get(i).getText().replaceAll(" ₽", "")));
+        }
+        int minPrice = listOfPrices.stream().min(Integer::compare).get();
+        return minPrice;
+    }
+
+    int getFirstElementPrice() {
+        int firstElementPrice = Integer.parseInt($$(AppiumBy.xpath("//*[@resource-id='ru.sportmaster.app:id/textViewMainPrice']"))
+                .get(0).getText().replaceAll(" ₽", ""));
+        return firstElementPrice;
     }
 }
--- src/test/java/ru/dubograev/tests/SwipeTest.java
+++ src/test/java/ru/dubograev/tests/SwipeTest.java
@@ -1,26 +1,64 @@
 package ru.dubograev.tests;
 
-import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.Arrays;
-
-import static com.codeborne.selenide.Selenide.$;
+import java.util.concurrent.TimeUnit;
 import static io.qameta.allure.Allure.step;
+import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 
-public class SwipeTest extends TestBase{
+public class SwipeTest {
+    AppiumDriver driver;
+
+    @BeforeEach
+    public void setup(){
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        File app = getApp();
+
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.merge(capabilities);
+        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+        options.setPlatformName("Android");
+        options.setDeviceName("Pixel 2 XL API 30");
+        options.setPlatformVersion("11.0");
+        options.setApp(app.getAbsolutePath());
+        options.setAppPackage("ru.sportmaster.app");
+        options.setAppActivity("ru.sportmaster.app.presentation.start.StartActivity");
+
+        try {
+            driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub/"), options);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        driver.manage().timeouts().implicitlyWait(6000, TimeUnit.MILLISECONDS);
+    }
+
     @Test
     @DisplayName("Swipe test")
-    void swipe(AppiumDriver driver) {
+    void horizontalScroll() {
         step("Check that text 'ВЕСЬ КАТАЛОГ В ТВОЁМ МОБИЛЬНОМ ПРИЛОЖЕНИИ' is displayed", () -> {
-            $(AppiumBy.id("ru.sportmaster.app:id/textViewTitle")).shouldHave(Condition.text("ВЕСЬ КАТАЛОГ В ТВОЁМ МОБИЛЬНОМ ПРИЛОЖЕНИИ"));
+            WebElement element = driver.findElement(AppiumBy.id("ru.sportmaster.app:id/textViewTitle"));
+            Assertions.assertThat(element.getText()).isEqualTo("ВЕСЬ КАТАЛОГ В\n" +
+                    "ТВОЁМ МОБИЛЬНОМ\n" +
+                    "ПРИЛОЖЕНИИ");
         });
 
         step("Swipe the screen from right to the left", () -> {
@@ -39,7 +77,30 @@ public class SwipeTest extends TestBase{
         });
 
         step("Check that text 'КОПИ БОНУСЫ И ОПЛАЧИВАЙ ИМИ ДО 30% ПОКУПКИ' is displayed", () -> {
-            $(AppiumBy.id("ru.sportmaster.app:id/textViewTitle")).shouldHave(Condition.text("КОПИ БОНУСЫ И ОПЛАЧИВАЙ ИМИ ДО 30% ПОКУПКИ"));
+            WebElement element = driver.findElement(AppiumBy.id("ru.sportmaster.app:id/textViewTitle"));
+            Assertions.assertThat(element.getText()).isEqualTo("КОПИ БОНУСЫ И\n" +
+                    "ОПЛАЧИВАЙ ИМИ ДО\n" +
+                    "30% ПОКУПКИ");
         });
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+    }
+
+    private File getApp() {
+        String appUrl = "https://github.com/dubograev/test-task-sc/blob/master/build/resources/test/app/sportm.apk";
+        String appPath = "src/test/resources/app/sportm.apk";
+
+        File app = new File(appPath);
+        if (!app.exists()) {
+            try (InputStream in = new URL(appUrl).openStream()) {
+                copyInputStreamToFile(in, app);
+            } catch (IOException e) {
+                throw new AssertionError("Failed to download application", e);
+            }
+        }
+        return app;
     }
 }
